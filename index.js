8f7e7db8baf9b4bbe357660d74bb110174ee145e
vishal sharma <vishallsharma07@gmail.com>
2021-12-12 10:53:18 +0530

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app  = express()
+const path = require('path')
+require('dotenv').config()
+app.use(express.static(path.join(__dirname, "views")));
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log("Server working at", process.env.PORT)
+})
